5c9b033a5beb20d13b0a56729adf14631db0c16f
Enrico Testa <enrico@mailtesta.com>
2021-11-02 17:35:34 +0100

    add get_vault_args()

--- utils.py
+++ utils.py
@@ -7,6 +7,40 @@ import os
 with open(os.path.join(os.path.dirname(__file__), 'defaults.yaml'), 'r') as f:
     DEFAULTS = safe_load(f)
 
+def get_vault_args():
+    """Argument parser.
+    Returns:
+      Dictionary of arguments.
+    """
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--input-directory',
+        default= DEFAULTS['vault']['input_directory'],
+        metavar='input_directory',
+        help='Directory where inputs are stored.')
+    parser.add_argument(
+        '--output-directory',
+        default= DEFAULTS['vault']['output_directory'],
+        metavar='output_directory',
+        help='Directory where outputs are stored.')
+    parser.add_argument(
+        '--custom-code-directory',
+        default= DEFAULTS['vault']['custom_code_directory'],
+        metavar='custom_code_directory',
+        help='Directory containing custom code.')
+    parser.add_argument(
+        '--log-directory',
+        default= DEFAULTS['vault']['log_directory'],
+        metavar='log_directory',
+        help='Directory containing logs.')
+    parser.add_argument(
+        '--run-mode',
+        default=DEFAULTS['vault']['run_mode'],
+        metavar='run_mode',
+        help='Directory where outputs are stored.')
+
+    return vars(parser.parse_args())
+
 def get_codebox_args():
     """Argument parser.
     Returns:
@@ -15,47 +49,47 @@ def get_codebox_args():
     parser = ArgumentParser()
     parser.add_argument(
         '--remote-input-directory',
-        default= DEFAULTS['remote_input_directory'],
+        default= DEFAULTS['codebox']['remote_input_directory'],
         metavar='remote_input_directory',
         help='Directory where remote inputs are stored.')
     parser.add_argument(
         '--remote-output-directory',
-        default= DEFAULTS['remote_output_directory'],
+        default= DEFAULTS['codebox']['remote_output_directory'],
         metavar='remote_output_directory',
         help='Directory where remote outputs are stored.')
     parser.add_argument(
         '--remote-custom-code-directory',
-        default= DEFAULTS['remote_custom_code_directory'],
+        default= DEFAULTS['codebox']['remote_custom_code_directory'],
         metavar='remote_custom_code_directory',
         help='Directory where remote custom code is stored.')
     parser.add_argument(
         '--remote-log-directory',
-        default= DEFAULTS['remote_log_directory'],
+        default= DEFAULTS['codebox']['remote_log_directory'],
         metavar='remote_log_directory',
         help='Directory containing execution logs.')
     parser.add_argument(
         '--input-directory',
-        default= DEFAULTS['input_directory'],
+        default= DEFAULTS['codebox']['input_directory'],
         metavar='input_directory',
         help='Directory where inputs are stored.')
     parser.add_argument(
         '--output-directory',
-        default= DEFAULTS['output_directory'],
+        default= DEFAULTS['codebox']['output_directory'],
         metavar='output_directory',
         help='Directory where outputs are stored.')
     parser.add_argument(
         '--custom-code-directory',
-        default= DEFAULTS['custom_code_directory'],
+        default= DEFAULTS['codebox']['custom_code_directory'],
         metavar='custom_code_directory',
         help='Directory containing custom code.')
     parser.add_argument(
         '--log-directory',
-        default= DEFAULTS['log_directory'],
+        default= DEFAULTS['codebox']['log_directory'],
         metavar='log_directory',
         help='Directory containing execution logs.')
     parser.add_argument(
         '--run-mode',
-        default=DEFAULTS['run_mode'],
+        default=DEFAULTS['codebox']['run_mode'],
         metavar='run_mode',
         help='Directory where outputs are stored.')
 

04c341b6511faedfa1636a99262aa01658e2e948
Enrico Testa <enrico@mailtesta.com>
2021-11-03 10:10:36 +0100

    add exception logs on sync_directory()

--- codebox.py
+++ codebox.py
@@ -16,18 +16,24 @@ logger.info("Logging started...")
 
 # STEP 1 - Retrieve remote input directory
 logger.info("Syncing input directory...")
-sync_directory(source_directory=args_dict['remote_input_directory'], destination_directory=args_dict['input_directory'])
+status = sync_directory(source_directory=args_dict['remote_input_directory'], destination_directory=args_dict['input_directory'])
+if status.returncode != 0:
+    logger.error(f"Return code {status.returncode}. Stderr: {status.stderr}")
+    raise ChildProcessError
 
 # STEP 2 - Retrieve custom code
 logger.info("Syncing custom code directory...")
-sync_directory(source_directory=args_dict['remote_custom_code_directory'], destination_directory=args_dict['custom_code_directory'])
+status = sync_directory(source_directory=args_dict['remote_custom_code_directory'], destination_directory=args_dict['custom_code_directory'])
+if status.returncode != 0:
+    logger.error(f"Return code {status.returncode}. Stderr: {status.stderr}")
+    raise ChildProcessError
+
 
 # STEP 3 - pip install to ensure requirements are fullfilled
 logger.info("Installing custom code...")
 pip_cmd = f"python -m pip install {args_dict['custom_code_directory']}"
-run(pip_cmd, shell=True)
+pip_process = run(pip_cmd, shell=True, capture_output=True)
 
-# TODO: remove all internet and network access @ this point. Either spawn process as another user or use iptables on current user.
 
 input_dir_arg = f"--input-directory={args_dict['input_directory']}"
 output_dir_arg = f"--output-directory={args_dict['output_directory']}"
@@ -36,10 +42,21 @@ vault_log_dir_arg = f"--log-directory={args_dict['vault_log_directory']}"
 run_mode_arg = f"--run-mode={args_dict['run_mode']}"
 
 vault_cmd = f"python vault.py {input_dir_arg} {output_dir_arg} {custom_code_dir_arg} {vault_log_dir_arg} {run_mode_arg}"
-vault_process = run(vault_cmd, shell=True, capture_output=True)
+logger.info("Run custom code in vault...")
 
+# TODO: remove all internet and network access @ this point. Either spawn process as another user or use iptables on current user.
+vault_process = run(vault_cmd, shell=True, capture_output=True)
 # TODO: restore network access @ this point
 
 # STEP 6 - Export outputs
-sync_directory(source_directory=args_dict['output_directory'], destination_directory=args_dict['remote_output_directory'])
-sync_directory(source_directory=args_dict['log_directory'], destination_directory=args_dict['remote_log_directory'])
+logger.info("Syncing output directory with remote path...")
+status = sync_directory(source_directory=args_dict['output_directory'], destination_directory=args_dict['remote_output_directory'])
+if status.returncode != 0:
+    logger.error(f"Return code {status.returncode}. Stderr: {status.stderr}")
+    raise ChildProcessError
+
+logger.info("Syncing log directory with remote path...")
+status = sync_directory(source_directory=args_dict['log_directory'], destination_directory=args_dict['remote_log_directory'])
+if status.returncode != 0:
+    logger.error(f"Return code {status.returncode}. Stderr: {status.stderr}")
+    raise ChildProcessError

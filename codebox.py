9830192c20cf4151ca28ad99160010faa5123f92
Enrico Testa <enrico@mailtesta.com>
2021-11-06 10:12:40 +0100

    add log export upon exception

--- codebox.py
+++ codebox.py
@@ -24,6 +24,7 @@ logger.info("Syncing input directory...")
 status = sync_directory(source_directory=args_dict['remote_input_directory'], destination_directory=args_dict['input_directory'])
 if status.returncode != 0:
     logger.error(f"Return code {status.returncode}. Stderr: {status.stderr}")
+    sync_directory(source_directory=args_dict['log_directory'], destination_directory=args_dict['remote_log_directory'])
     raise ChildProcessError
 
 # STEP 2 - Retrieve custom code
@@ -31,6 +32,7 @@ logger.info("Syncing custom code directory...")
 status = sync_directory(source_directory=args_dict['remote_custom_code_directory'], destination_directory=args_dict['custom_code_directory'])
 if status.returncode != 0:
     logger.error(f"Return code {status.returncode}. Stderr: {status.stderr}")
+    sync_directory(source_directory=args_dict['log_directory'], destination_directory=args_dict['remote_log_directory'])
     raise ChildProcessError
 
 
@@ -41,6 +43,11 @@ if status.returncode != 0:
 logger.info("Installing custom code...")
 pip_cmd = f"sudo -H python -m pip install -r {os.path.join(args_dict['custom_code_directory'], 'requirements.txt')}; sudo -H python -m pip install {args_dict['custom_code_directory']}"
 pip_process = run(pip_cmd, shell=True, capture_output=True)
+if pip_process.returncode != 0:
+    logger.error("Vault process failed. Stderr: ")
+    logger.error(f"{pip_process.stderr.decode('utf-8')}")
+    sync_directory(source_directory=args_dict['log_directory'], destination_directory=args_dict['remote_log_directory'])
+    raise ChildProcessError
 
 input_dir_arg = f"--input-directory={args_dict['input_directory']}"
 output_dir_arg = f"--output-directory={args_dict['output_directory']}"
@@ -53,18 +60,21 @@ vault_cmd = f"sudo -u vault_user {python_cmd}"
 logger.info("Run custom code in vault...")
 
 # Remove all internet and network access for vault_user (uid 1500)
+# Cannot disable gsutil for specific user is not possible
 logger.info("Configuring iptables...")
 iptables_cmd = "sudo iptables -A OUTPUT -m owner --uid-owner 1500 -j DROP"
 iptables_process = run(iptables_cmd, shell=True, capture_output=True)
 if iptables_process.returncode != 0:
     logger.error("Failed to configure iptables.")
     logger.error(f"{iptables_process.stderr.decode('utf-8')}")
+    sync_directory(source_directory=args_dict['log_directory'], destination_directory=args_dict['remote_log_directory'])
     raise ChildProcessError
 
 vault_process = run(vault_cmd, shell=True, capture_output=True)
 if vault_process.returncode != 0:
     logger.error("Vault process failed. Stderr: ")
     logger.error(f"{vault_process.stderr.decode('utf-8')}")
+    sync_directory(source_directory=args_dict['log_directory'], destination_directory=args_dict['remote_log_directory'])
     raise ChildProcessError
 
 # STEP 6 - Export outputs
@@ -72,6 +82,7 @@ logger.info("Syncing output directory with remote path...")
 status = sync_directory(source_directory=args_dict['output_directory'], destination_directory=args_dict['remote_output_directory'])
 if status.returncode != 0:
     logger.error(f"Return code {status.returncode}. Stderr: {status.stderr}")
+    sync_directory(source_directory=args_dict['log_directory'], destination_directory=args_dict['remote_log_directory'])
     raise ChildProcessError
 
 logger.info("Syncing log directory with remote path...")
